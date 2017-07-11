9abd0098471afedb77b2ee42a3ab82b750c82b3b
iD Student <idstudent@iDs-iMac.local>
2017-07-11 16:14:23 -0700

    Initial Commit

---
+++ Encrypt Any Text/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Encrypt Any Text
+//
+//  Created by iD Student on 7/11/17.
+//  Copyright © 2017 com.idtech.daniel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
